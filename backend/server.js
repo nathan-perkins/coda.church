9b362193753a36e0cd972e6fe6d87ce824082362
nathan-perkins <140360768+nathan-perkins@users.noreply.github.com>
2025-07-28 18:59:04 -0500

    feat: connect backend to database

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Server is ready")
+})
+
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`Server started at http://localhost:${PORT}`)
+})
